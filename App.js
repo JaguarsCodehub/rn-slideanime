5f10fc91743cebe3828df0522f6bfb300b4f1672
JyotindraTavanoji <jktbusiness21@gmail.com>
2022-03-04 14:26:38 +0530

    'slide-anime'

--- App.js
+++ App.js
@@ -1,21 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from 'react';
+import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+const { width, height } = Dimensions.get('screen');
+
+const data = [
+    'https://wallpaperaccess.com/download/sasuke-phone-1542102',
+    'https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200',
+    'https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200',
+    'https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200',
+    'https://wallpaperaccess.com/download/sasuke-phone-1645647',
+    'https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200',
+    'https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200',
+
+];
+
+
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+const App = () => {
+
+    const scrollX = React.useRef(new Animated.Value(0)).current;
+
+    return (
+        <View style={{ flex: 1, backgroundColor: '#000' }}>
+            <StatusBar hidden />
+            <View
+                style={StyleSheet.absoluteFillObject}
+            >
+                {data.map((image,index) => {
+
+                    const inputRange = [
+                        (index - 1)* width,
+                        index * width,
+                        (index + 1)* width
+                    ]
+                    const opacity = scrollX.interpolate({
+                        inputRange,
+                        outputRange: [0,1,0]
+                    })
+                    return (
+                        <Animated.Image 
+                            key={`image-${index}`}
+                            source={{uri: image}}
+                            style={[
+                                StyleSheet.absoluteFillObject,
+                                {
+                                    opacity
+                                }
+                            ]}
+                            blurRadius={10}
+                        />
+                        )
+                })}
+            </View>
+            <Animated.FlatList 
+                data={data}
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                    { useNativeDriver: true}
+                )}
+                horizontal
+                pagingEnabled
+                keyExtractor={(_, index) => index.toString()}
+                renderItem={({item}) => {
+                    return ( 
+                        <View style={{
+                            width,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                               
+                        }}>
+                            <Image 
+                                source={{uri: item}} style={{
+                                    width: imageW,
+                                    height: imageH,
+                                    resizeMode: 'cover',
+                                    borderRadius: 20,
+                                    shadowColor: '#000',
+                            shadowOpacity: 10,
+                            shadowOffset: {
+                                width: 0,
+                                height: 0,
+                            },
+                            shadowRadius: 20 
+                                }}
+                            />
+                        </View>
+                    )
+                }}
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ react-native-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    projects: {
+        ios: {},
+        android: {}
+    },
+    asserts: ["./assets/fonts/"]
+}
